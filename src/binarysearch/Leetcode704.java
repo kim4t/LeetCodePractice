49829a66ebe6cd0ebef2e2daad7b5dad92bad29b
Taeho <tkim@svb.com>
2022-06-14 12:02:45 -0700

    704

--- src/binarysearch/Leetcode704.java
+++ src/binarysearch/Leetcode704.java
@@ -1,7 +1,18 @@
 package binarysearch;
 
 public class Leetcode704 {
-    public static void main(String[] args) {
-
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid]>target)
+                r = mid-1;
+            else
+                l = mid+1;
+        }
+        return -1;
     }
 }
